bf0acb82873ffc4813c80219971ee12b6206d20a
Matthew Gsell <matthewdavidgsell@gmail.com>
2023-12-23 18:07:15 -0500

    imported icons

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,10 +1,16 @@
 
 import { Link } from "react-router-dom"
 import LandingCss from './Landing.module.css'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faRedhat } from "@fortawesome/free-brands-svg-icons"
+import { faShoePrints, faShirt } from "@fortawesome/free-solid-svg-icons"
 function Landing () {
     return (
         <div className={LandingCss.landingpagemain}>
     <div><h1>The Shoe Hat and Shirt Store</h1>
+    <FontAwesomeIcon icon={faRedhat} />
+    <FontAwesomeIcon icon={faShoePrints} />
+    <FontAwesomeIcon icon={faShirt} />
 </div>
     <h3>You won't be able to guess what products we sell!</h3>
     <h6>Click shop no to see our collecton</h6>
